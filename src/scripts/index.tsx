1a297eb145ad28df93ffc89a524f986f3152a808
Aleksandr Chernikov <AYuChernikov.SBT@sberbank.ru>
2016-04-13 11:57:18 +0300

    невозможный общий коммит

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import {Autocomplete} from '../autocomplete/index';
+
 import Input from 'Components/Input';
 
-ReactDOM.render(<Input />, document.getElementById('app'));
+ReactDOM.render(
+    <Autocomplete
+        fetch={search => new Promise<any>((resolve) => resolve([]))}
+        valueLink={() => false}
+    />,
+    document.getElementById('app'));
